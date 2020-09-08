a2ddc9f47ede29cd53073c77f0cec9ad6d036f10
Robin Knol <robin@knol.email>
2020-09-08 16:12:16 +0200

    Discoverd there was more than I thought there was

---
+++ Assets/Script/Opdracht/Path.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Opdrachten
+{
+    /// <summary>
+    /// De path class beheerd een array van waypoints. En houd bij bij welk waypoint een object is.
+    /// Deze vormen samen het pad. 
+    /// Logica die op het path niveau plaatsvindt gebeurt in deze class.
+    /// Een deel van de functies welke je nodig hebt zijn hier al beschreven.
+    /// </summary>
+    public class Path : MonoBehaviour
+    {
+
+
+        /// <summary>
+        /// Deze functie returned het volgende waypoint waar naartoe kan worden bewogen.
+        /// </summary>
+        public Waypoint GetNextWaypoint()
+        {
+
+            // dit is nu niet goed, zorg ervoor dat het goede waypoint wordt teruggegeven.
+            return null;
+        }
+    }
+}
---
+++ Assets/Script/Opdracht/PathFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Opdrachten
+{
+    /// <summary>
+    /// De path follower class is verantwoordelijk voor de beweging.
+    /// Deze class zorgt ervoor dat het object (in Tower Defense) vaak een enemy, het path afloopt
+    /// tip: je kunt de transform.LookAt() functie gebruiken en vooruitbewegen.
+    /// </summary>
+    public class PathFollower : MonoBehaviour
+    {
+
+    }
+}
