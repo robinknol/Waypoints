a058a3916fe10a1ded7f51ad1b12639881fdb3a6
Robin Knol <robin@knol.email>
2020-09-08 12:23:47 +0200

    Initial commit

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    public GameObject Waypoint;
+    private int maxTime = 250;
+    public int timer;
+    void Start()
+    {
+        timer = maxTime;
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    void Update()
+    {
+        timer -= 1;
+        if(timer == 0)
+        {
+            Vector3 location = Waypoint.transform.position;
+            location.x = location.x += 1;
+            location.z = location.z += 1;
+            Debug.Log(location);
+            gameObject.transform.Translate(location);
+            timer = maxTime;
+        }
+    }
+}
---
+++ Assets/RandomWaypointSpawnlocation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomWaypointSpawnlocation : MonoBehaviour
+{
+    public float x;
+    void Start()
+    {
+        x = Random.Range(-10f, 10f);
+
+        gameObject.transform.Translate(x,0,x);
+    }
+}
